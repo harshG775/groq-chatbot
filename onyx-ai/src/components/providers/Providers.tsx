0aa04c0aa10d1bb30ffffaf95cc329bc30b818be
Harsh <hgaur491@gmail.com>
2024-09-08 13:24:00 +0530

    ///

--- onyx-ai/src/components/providers/Providers.tsx
+++ onyx-ai/src/components/providers/Providers.tsx
@@ -1,18 +1,21 @@
 import { MessagesProvider } from "@/store/context/Messages-context";
 import { ThemeProvider } from "./theme-provider";
+import { UserInputProvider } from "@/store/context/UserInput-context";
 
 type ProvidersPropsType = { children: React.ReactNode };
 export default function Providers({ children }: ProvidersPropsType) {
     return (
-        <MessagesProvider>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange={true}
-            >
-                {children}
-            </ThemeProvider>
-        </MessagesProvider>
+        <UserInputProvider>
+            <MessagesProvider>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange={true}
+                >
+                    {children}
+                </ThemeProvider>
+            </MessagesProvider>
+        </UserInputProvider>
     );
 }
