9e8e6495b7e098f18b07ae6d62788754585abfbc
Harsh <hgaur491@gmail.com>
2024-09-08 13:23:48 +0530

    creating contexts for messages and userInput

--- onyx-ai/src/components/providers/Providers.tsx
+++ onyx-ai/src/components/providers/Providers.tsx
@@ -1,15 +1,18 @@
+import { MessagesProvider } from "@/store/context/Messages-context";
 import { ThemeProvider } from "./theme-provider";
 
 type ProvidersPropsType = { children: React.ReactNode };
 export default function Providers({ children }: ProvidersPropsType) {
     return (
-        <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange={true}
-        >
-            {children}
-        </ThemeProvider>
+        <MessagesProvider>
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+                disableTransitionOnChange={true}
+            >
+                {children}
+            </ThemeProvider>
+        </MessagesProvider>
     );
 }
---
+++ onyx-ai/src/store/context/Messages-context.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React, {
+    createContext,
+    useContext,
+    useState,
+    ReactNode,
+    Dispatch,
+    SetStateAction,
+} from "react";
+export type message = {
+    role: "user" | "assistant" | "system";
+    content: string;
+};
+
+type MessagesContextType = {
+    messages: message[];
+    setMessage: Dispatch<SetStateAction<message[]>>;
+};
+
+// Create the context with a default value
+const MessagesContext = createContext<MessagesContextType | null>(null);
+
+// Create a custom hook to use the MessagesContext
+export const useMessagesContext = (): MessagesContextType => {
+    const context = useContext(MessagesContext);
+    if (!context) {
+        throw new Error(
+            "useMessagesContext must be used within a MessagesProvider"
+        );
+    }
+    return context;
+};
+
+// Create a provider component
+type MessagesProviderProps = {
+    children: ReactNode;
+};
+
+export function MessagesProvider({ children }: MessagesProviderProps) {
+    const [messages, setMessage] = useState<message[]>([]);
+    return (
+        <MessagesContext.Provider value={{ messages, setMessage }}>
+            {children}
+        </MessagesContext.Provider>
+    );
+}
