a15ebe00b27aeb8dc7186ca9cfadce02f1a777eb
Harsh <hgaur491@gmail.com>
2024-09-08 12:43:12 +0530

    ///

---
+++ onyx-ai/src/app/(pages)/chat/page.tsx
@@ -0,0 +1,7 @@
+export default function ChatPage() {
+    return (
+        <main className="container grid place-content-center gap-2 h-screen text-center">
+            <h1 className="text-2xl font-bold text-primary">Chat page</h1>
+        </main>
+    );
+}
