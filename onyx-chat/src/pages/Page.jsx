7b7a0ea875d8d192d8dc698563932690d77a06c6
Harsh <hgaur491@gmail.com>
2024-09-22 11:52:54 +0530

    #test app canceling/aborting request

--- onyx-chat/src/pages/Page.jsx
+++ onyx-chat/src/pages/Page.jsx
@@ -1,18 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { getGroqChatCompletion } from "@/services/groq/groq";
-import { useMessagesContext } from "@/store/context/Messages-context";
-import { useStreamMessageContext } from "@/store/context/StreamMessage-context";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function HomePage() {
-    const { messages, setMessages } = useMessagesContext();
+    const [messages, setMessages] = useState([]);
+    const controller = useRef(null);
     useEffect(() => {
-        setMessages(histories[0].history);
+        setMessages([
+            {
+                role: "system",
+                content: "You are a coding assistant.",
+            },
+        ]);
     }, [setMessages]);
 
     const [streaming, setStreaming] = useState(false);
-    const { streamMessage, setStreamMessage } = useStreamMessageContext();
-    const handleSend = async () => {
+    const [streamMessage, setStreamMessage] = useState();
+    const handleSend = async (input) => {
         try {
             setStreamMessage({
                 content: "",
@@ -21,18 +25,20 @@ export default function HomePage() {
             setMessages((prev) => [
                 ...prev,
                 {
-                    content: "hello",
+                    content: input,
                     role: "user",
                 },
             ]);
             const stream = await getGroqChatCompletion({
-                query: "hello",
+                query: input,
                 history: messages,
                 model: "llama-3.1-8b-instant",
             });
+            controller.current = stream.controller;
             setStreaming(true);
             let accumulated = "";
             for await (const { choices } of stream) {
+                await delay(40);
                 if (choices[0]?.finish_reason === "stop") {
                     setMessages((prev) => [
                         ...prev,
@@ -54,20 +60,38 @@ export default function HomePage() {
             console.log(error);
         }
     };
-    console.log(streaming);
 
     return (
         <>
             <main className="mx-auto max-w-2xl min-h-96 border p-2">
+                <section>
+                    <nav>
+                        <input type="text" className="w-full min-h-12" />
+                        <Button onClick={() => handleSend("react counter")}>send</Button>
+                        <Button
+                            onClick={() => {
+                                setStreaming(false);
+                                setMessages((prev) => [
+                                    ...prev,
+                                    {
+                                        content: streamMessage.content,
+                                        role: "assistant",
+                                    },
+                                ]);
+                                controller.current.abort();
+                            }}
+                        >
+                            cancel
+                        </Button>
+                    </nav>
+                </section>
                 <div>
                     {messages?.map((el, i) => (
                         <div key={i}>
                             {el.role === "user" && (
                                 <>
                                     <div className="flex justify-end mt-4">
-                                        <div className="bg-cyan-600 text-orange-50 px-4 rounded-md">
-                                            user
-                                        </div>
+                                        <div className="bg-cyan-600 text-orange-50 px-4 rounded-md">user</div>
                                     </div>
                                     <p className="text-end">{el.content}</p>
                                 </>
@@ -75,9 +99,7 @@ export default function HomePage() {
                             {el.role === "assistant" && (
                                 <>
                                     <div className="flex mt-4">
-                                        <div className="bg-orange-600 text-orange-50 px-4 rounded-md">
-                                            assistant
-                                        </div>
+                                        <div className="bg-orange-600 text-orange-50 px-4 rounded-md">assistant</div>
                                     </div>
                                     <p>{el.content}</p>
                                 </>
@@ -87,65 +109,14 @@ export default function HomePage() {
                     {streaming && (
                         <div>
                             <div className="flex mt-4">
-                                <div className="bg-orange-600 text-orange-50 px-4 rounded-md">
-                                    {streamMessage.role}
-                                </div>
+                                <div className="bg-orange-600 text-orange-50 px-4 rounded-md">{streamMessage.role}</div>
                             </div>
                             <p>{streamMessage?.content}</p>
                         </div>
                     )}
                 </div>
             </main>
-            <section>
-                <nav>
-                    <input type="text" className="w-full min-h-12" />
-                    <Button onClick={handleSend}>send</Button>
-                </nav>
-            </section>
         </>
     );
 }
-
-const histories = [
-    {
-        conversation_id: "1",
-        history: [
-            {
-                role: "system",
-                content: "You are a coding assistant.",
-            },
-            {
-                role: "user",
-                content: "Explain the use of functions in Python.",
-            },
-            {
-                role: "assistant",
-                content:
-                    "Functions in Python are blocks of reusable code that perform a specific task.",
-            },
-        ],
-        timestamp: "2024-09-21T10:00:00Z",
-    },
-    {
-        conversation_id: "2",
-        history: [
-            {
-                role: "system",
-                content:
-                    "You are an assistant that answers general knowledge questions.",
-            },
-            {
-                role: "user",
-                content: "What is the capital of France?",
-            },
-            {
-                role: "assistant",
-                content: "The capital of France is Paris.",
-            },
-        ],
-        timestamp: "2024-09-22T11:30:00Z",
-    },
-];
-
-
-const delay = (ms)=> new Promise(resolve => setTimeout(resolve, ms));
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
