3195411681664e403a823b2e5e0454547c019e2c
Harsh Gaur <hgaur491@gmail.com>
2025-02-08 16:48:23 +0530

    //

--- src/services/groq/index.ts
+++ src/services/groq/index.ts
@@ -1,9 +1,4 @@
 import config from "@/config";
 import Groq from "groq-sdk";
 const apiKey = config.services.ai.groqSDK.apiKey;
-console.log(apiKey);
-
-export const groqClient = new Groq({
-    dangerouslyAllowBrowser: true,
-    apiKey: apiKey,
-});
+export const groqClient = new Groq({ dangerouslyAllowBrowser: true, apiKey: apiKey });
