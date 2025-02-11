5afd7bff90d215a6a93368fa72f10fd0dfcc7366
Harsh Gaur <hgaur491@gmail.com>
2025-02-11 12:44:08 +0530

    fixing showing streaming:true after streaming ends

--- src/services/groq/useSolveQuery.ts
+++ src/services/groq/useSolveQuery.ts
@@ -61,6 +61,7 @@ export default function useSolveQuery({ userPrompt }: { userPrompt: string }): {
                     content: accumulated,
                     streaming: false,
                 });
+                return;
             }
             // start
             setStreamMessage({
