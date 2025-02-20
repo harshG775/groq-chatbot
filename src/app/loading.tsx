e4418141f5b1dbd5366af4b6203e09a751ab90cc
Harsh Gaur <hgaur491@gmail.com>
2025-02-20 11:00:43 +0530

    loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,4 +1,4 @@
-export function PageLoading() {
+export function PageLoadingBasic() {
     return (
         <div className=" flex space-x-2 justify-center items-center">
             <span className="sr-only">Loading...</span>
@@ -8,11 +8,29 @@ export function PageLoading() {
         </div>
     );
 }
-
+export function PageLoadingBounceSpin() {
+    return (
+        <div className="flex gap-2 items-center justify-center h-screen">
+            <span className="sr-only">Loading...</span>
+            <div className="relative animate-bounce [animation-delay:-0.3s]">
+                <div className="h-10 w-10 rounded-full border-t-4 border-b-4 border-primary animate-bounce [animation-delay:-0.3s]"></div>
+                <div className="h-10 w-10 rounded-full border-t-4 border-b-4 border-primary animate-spin"></div>
+            </div>
+            <div className="relative animate-bounce [animation-delay:-0.15s]">
+                <div className="h-10 w-10 rounded-full border-t-4 border-b-4 border-primary animate-bounce [animation-delay:-0.15s]"></div>
+                <div className="h-10 w-10 rounded-full border-t-4 border-b-4 border-primary animate-spin"></div>
+            </div>
+            <div className="relative animate-bounce">
+                <div className="h-10 w-10 rounded-full border-t-4 border-b-4 border-primary animate-bounce "></div>
+                <div className="h-10 w-10 rounded-full border-t-4 border-b-4 border-primary animate-spin"></div>
+            </div>
+        </div>
+    );
+}
 export default function loading() {
     return (
         <div className="fixed inset-0 grid place-content-center bg-background">
-            <PageLoading />
+            <PageLoadingBounceSpin />
         </div>
     );
 }
