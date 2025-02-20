b0156e53aa5cd1396baf9bd0b72c811b4eea4cae
Harsh Gaur <hgaur491@gmail.com>
2025-02-20 10:40:34 +0530

    adding loading for full page

---
+++ src/app/loading.tsx
@@ -0,0 +1,18 @@
+export function PageLoading() {
+    return (
+        <div className=" flex space-x-2 justify-center items-center">
+            <span className="sr-only">Loading...</span>
+            <div className="h-8 w-8 bg-foreground rounded-full animate-bounce [animation-delay:-0.3s]"></div>
+            <div className="h-8 w-8 bg-foreground rounded-full animate-bounce [animation-delay:-0.15s]"></div>
+            <div className="h-8 w-8 bg-foreground rounded-full animate-bounce"></div>
+        </div>
+    );
+}
+
+export default function loading() {
+    return (
+        <div className="fixed inset-0 grid place-content-center bg-background">
+            <PageLoading />
+        </div>
+    );
+}
